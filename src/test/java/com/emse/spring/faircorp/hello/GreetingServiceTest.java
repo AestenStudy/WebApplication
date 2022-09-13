7be0b842c4ada9475365f01157e792661bde4061
arcady ruvidic <liliasfhaleaf@gmail.com>
2022-09-13 09:40:52 +0200

    Greeting service added

---
+++ src/test/java/com/emse/spring/faircorp/hello/GreetingServiceTest.java
@@ -0,0 +1,18 @@
+package com.emse.spring.faircorp.hello;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+
+@ExtendWith(OutputCaptureExtension.class)
+class GreetingServiceTest {
+
+    @Test
+    public void testGreeting(CapturedOutput output) {
+        GreetingService greetingService = new ConsoleGreetingService();
+        greetingService.greet("Spring");
+        Assertions.assertThat(output.getAll()).contains("Hello, Spring!");
+    }
+}
