d240036cfb30f36cf17e4a92ed4bf902da0c1810
arcady ruvidic <liliasfhaleaf@gmail.com>
2022-10-04 11:22:56 +0200

    modified mapping

--- src/main/java/com/emse/spring/faircorp/api/SecurityController.java
+++ src/main/java/com/emse/spring/faircorp/api/SecurityController.java
@@ -6,8 +6,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("api/security")
 public class SecurityController {
 
     @Secured("ROLE_ADMIN")
